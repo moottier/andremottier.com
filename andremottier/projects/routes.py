d28b53071667233efae859ec5b9fa7abdfe5cce7
Andre Mottier <me@andremottier.com>
2021-12-26 00:30:31 +0000

    moved blueprint setup into __init__.py

--- andremottier/projects/routes.py
+++ andremottier/projects/routes.py
@@ -1,19 +1,6 @@
-from flask import Blueprint, render_template, url_for
-from flask_login import login_required, current_user
-from andremottier.projects.projects.php_websocket_chat.routes import chat_server_php as chat_server_php_blueprint
-    
-
-name = 'projects'
-projects = Blueprint(
-    name=name, 
-    import_name=__name__,
-    static_folder='static',
-    static_url_path=f'/static',
-    url_prefix='/projects',
-    template_folder='templates',
-    root_path='/var/www/dev.andremottier.com/andremottier/projects'
-)
-projects.register_blueprint(chat_server_php_blueprint)
+from flask import render_template
+from flask_login import login_required
+from andremottier.projects import projects
 
 @projects.route('/')
 @login_required
