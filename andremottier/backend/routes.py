fdc4f354ff5a9feb27baede89180dbf101114d67
Andre Mottier <me@andremottier.com>
2022-02-05 00:53:30 +0000

    add basic sitemap

--- andremottier/backend/routes.py
+++ andremottier/backend/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app
 from flask_login import login_required, current_user
 from andremottier.backend import main
 
@@ -11,3 +11,11 @@ def index():
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.name)
+
+@main.route('/site-map')
+@login_required
+def site_map():
+    #site_map = [item.string for item in list(current_app.url_map.iter_rules())]
+    site_map = [(item.rule, item.methods, item.endpoint) for item in current_app.url_map.iter_rules()]
+    #site_map = dir(list(current_app.url_map.iter_rules())[0])
+    return render_template('site-map.html', site_map=site_map)
