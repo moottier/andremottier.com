667fc3365e6fee66171ceae83817f4fe8894b406
Andre Mottier <me@andremottier.com>
2022-02-26 23:06:16 +0000

    cleanup

--- andremottier/projects/__init__.py
+++ andremottier/projects/__init__.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from andremottier.projects.projects.php_websocket_chat.routes import chat_server_php as chat_server_php_blueprint
+from andremottier.projects.projects.image_resizer.routes import image_resizer as image_resizer_blueprint
 
 name = 'projects'
 projects = Blueprint(
@@ -12,3 +13,4 @@ projects = Blueprint(
     root_path='/var/www/dev.andremottier.com/andremottier/projects'
 )
 projects.register_blueprint(chat_server_php_blueprint)
+projects.register_blueprint(image_resizer_blueprint)
