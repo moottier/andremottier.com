8c6ebdcb52626b7cd4f34a037ddb34be8c03530d
Andre Mottier <me@andremottier.com>
2022-02-27 17:43:46 +0000

    server validation

--- andremottier/projects/projects/image_resizer/routes.py
+++ andremottier/projects/projects/image_resizer/routes.py
@@ -1,11 +1,29 @@
-from flask import render_template, url_for, jsonify, request, send_file
-import io
+from flask import flash, render_template, url_for, jsonify, request, send_file
+import io, sys
 from andremottier.projects.projects.image_resizer import image_resizer
 from .image_resizer import resizer
 
 from PIL import Image
 
 def validate_request():
+    if not hasattr(request, 'content_length'): return False
+    content_length = request.content_length or 0
+    
+    if not (0 < content_length < 5000000): return False
+
+    if not hasattr(request, 'form'): return False
+    width = request.form.get('width') or '0'
+    height = request.form.get('height') or '0'
+    
+    if not (width.isnumeric() and height.isnumeric()): return False
+    if not ((0 < len(width) <= 4) and (0 < len(height) <= 4)): return False
+
+    if not request.files.getlist('file'): return False
+    if not len(request.files.getlist('file')) == 1: return False
+    
+    file = request.files.getlist('file')[0]
+    if not resizer.get_PIL_format_from_mimetype(file.mimetype): return False
+    
     return True
 
 @image_resizer.route('/')
@@ -19,10 +37,19 @@ def index():
         styles=[],
     )
 
+def get_link(innerHTML, href, attributes=None):
+    """Return an <A> element with values passed
+    Attributes not implemented""" 
+    return f'<a href={href}>{innerHTML}</a>'
+
 @image_resizer.route('/submit', methods=['POST'])
 def submit():
-    if not validate_request():
-        pass    # error
+    if validate_request():
+        # print(validate_request)
+        # issue = get_link('issue', 'https://github.com/moottier/image_resizer/issues')
+        err_msg = ""
+        # flash(err_msg)    # need ajax
+        return err_msg, 500
     else:
         file = request.files.getlist('file')[0]
         size = (int(request.form['width']), int(request.form['height']))
